1db4ababc541c940407ee798ce7fb440199da618
David Perez <david.perez.dam2@gmail.com>
2024-01-04 11:48:28 +0100

    Initial Commit

---
+++ PokeApp_SwiftUI/PokeApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeApp_SwiftUIApp.swift
+//  PokeApp_SwiftUI
+//
+//  Created by David Perez on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct PokeApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
